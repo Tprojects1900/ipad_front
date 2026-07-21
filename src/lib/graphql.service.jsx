ef33c7c7aae9a1c6301dabccc9d05c76823f7252
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-21 19:05:54 +0000

    push

--- src/lib/graphql.service.jsx
+++ src/lib/graphql.service.jsx
@@ -4,12 +4,85 @@ import { GETAUTH ,TEAMS,GROUPS, SCHEDULESMATCH, EACHMATCHROSTER, SCHEDULELIVE, T
 import { ADDGROUP,ADDHALFTIME, ADDPLAYER, ADDSUB, ADDTEAM, ADDLABEL,UPDATELABEL, CHANGEEVENTSTATUS, DROPACTOR, DROPEVENT, EDITGROUPTEAMS, LOGIN,REMOVEGROUP,REMOVEMATCHROSTER,REMOVEPLAYER,REMOVESCHEDULE,REMOVETEAM, SAVEMATCHROSTER, SCHEDULEMATCH, STANDAREVENT, SWITCHMATCHMODE, TOGGLEDISABLE, UPDATEPLAYER, UPDATETEAM, UPDATETIMERORPLAYER } from "./graphql/mutation";
 import { LIVEMATCHEVENTS } from "./graphql/subscription";
 
+
+//LAZY QUERIES , {
+  //   fetchPolicy: "network-only", // ignore le cache
+  // }
+export const useGetAuth = () => {
+    return useLazyQuery(GETAUTH);
+}
+
+//QUERIES
+
+export const useGetTeams=()=>{
+   return useQuery(TEAMS);
+}
+
+export const useGetGroups=()=>{
+  return useQuery(GROUPS);
+}
+
+export const useGetSchedules = () => {
+  return useQuery(SCHEDULESMATCH);
+};
+
+export const useEachMatchRosters = (matchId) => {
+  return useQuery(EACHMATCHROSTER, {
+    variables: { matchId },
+    //  fetchPolicy: "network-only",
+    skip: !matchId,
+  });
+};
+//commentaire sur
+// // , {
+//     pollInterval: 3000, // ou 5000
+//     notifyOnNetworkStatusChange: true,
+//     fetchPolicy: "network-only",
+//   }
+export const useScheduleLive = () => {
+  return useQuery(SCHEDULELIVE);
+};
+
+export const useTeamStats=(teamId)=>{
+//  return useQuery(TEAMSTATS);
+    return useQuery(TEAMSTATS, {
+    variables: { teamId },
+    skip: !teamId,
+  });
+}
+
+export const useGetMatchById=(matchId)=>{
+//  return useQuery(TEAMSTATS);
+    return useQuery(GETMATCHBYID, {
+    variables: { getMatchByIdId:matchId },
+    skip: !matchId,
+  });
+}
+
+export const useGetTeamsDetails=(teamId)=>{
+//  return useQuery(TEAMSTATS);
+    return useQuery(TEAMDETAILS, {
+    variables: { getTeamDetailsId:teamId },
+    skip: !teamId,
+  });
+}
+
+
 export const useGetToggleSettings=()=>{
-  return {
-    data: {
-      getTopSettings: {}
+  return useQuery(GETTOGGLEDISABLE)
+}
+
+
+export const useGetCaisseDashboard=()=>{
+  return useQuery(CAISSEDASHBOARD)
+}
+
+export const useGetSingleRoster=(matchId,teamId)=>{
+  return useQuery(SINGLEROSTER,{
+    variables: {
+      matchId,
+      teamId
     },
-    loading:false,
-    refetchSettings:()=>{}
-  };
+     skip: !matchId || !teamId,
+  })
 }
