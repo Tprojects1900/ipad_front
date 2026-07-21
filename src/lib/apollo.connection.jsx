41eaf9f7ffae5117899c76e3e5a4c01cb6e8dcbb
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-21 23:13:28 +0000

    message dev

--- src/lib/apollo.connection.jsx
+++ src/lib/apollo.connection.jsx
@@ -1,29 +1,18 @@
-import { ApolloClient, InMemoryCache, split,HttpLink } from '@apollo/client';
-// import { setContext } from '@apollo/client/link/context';
-// import { getMainDefinition } from '@apollo/client/utilities';
-// import { GraphQLWsLink } from '@apollo/client/link/subscriptions'; 
-// import { createClient } from 'graphql-ws'; 
-//  import {createUploadLink} from 'apollo-upload-client';
-//  REFACTION ICI : Import standard compatible avec Webpack
-// import createUploadLink from 'apollo-upload-client/public/createUploadLink.js';
-import Cookies from 'js-cookie';
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-const isProd = typeof window !== "undefined" && window.location.hostname.includes("talent-hubapp.com");
+const isProd =
+  typeof window !== "undefined" &&
+  window.location.hostname.includes("talent-hubapp.com");
 
-const api_url = isProd 
-  ? "https://talent-hubapp.com/graphql" 
-  : "http://localhost:4000/graphql"; // Ajusté sur 5000 pour correspondre à ton local si besoin
-
-const api_wss_url = isProd 
-  ? "wss://talent-hubapp.com/graphql" 
-  : "ws://localhost:4000/graphql";
-
+const api_url = isProd
+  ? "https://talent-hubapp.com/graphql"
+  : "http://localhost:4000/graphql";
 
 const link = new HttpLink({
-    uri: api_url
+  uri: api_url,
 });
 
 export const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link
+  cache: new InMemoryCache(),
+  link,
 });
