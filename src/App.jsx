4a19ca2f3e7e1bb84c5bf3bdb939add9b9ff9094
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-21 18:46:23 +0000

    push

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ import { SwitchProvider } from './context/SwitchContext'
 export default function App() {
   return (
     <div style={{fontSize:"40px", color:"green"}}>
-      SANS PROVIDERS OK avec useToggle(apollo client 8)
+      SANS PROVIDERS OK avec useToggle(apollo client 9)
     </div>
   )
 }
