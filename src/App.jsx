3e73299d724af38027fd71517633cb362893e2b8
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-21 17:01:59 +0000

    sans provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
 import React from 'react'
-import { SwitchProvider } from './context/SwitchContext'
-// import AppRoutes from './routes'
-import { ScreenProvider } from './context/ScreenContext'
-import { SidebarProvider } from './context/SidebarContext'
-import AppProvider from './providers/app.provider'
+// import { SwitchProvider } from './context/SwitchContext'
+// // import AppRoutes from './routes'
+// import { ScreenProvider } from './context/ScreenContext'
+// import { SidebarProvider } from './context/SidebarContext'
+// import AppProvider from './providers/app.provider'
 
-// 1. IMPORTATION DES STYLES OBLIGATOIRE
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css' 
+// // 1. IMPORTATION DES STYLES OBLIGATOIRE
+// import { ToastContainer } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css' 
+
+// export default function App() {
+//   return (
+//     <AppProvider>
+//       <SidebarProvider>
+//         <ScreenProvider>
+//           <SwitchProvider>
+//             {/* <AppRoutes /> */}
+//             <div style={{fontSize:"40px", color:"green"}}>
+//               APP ROUTES TEST OK
+//             </div>
+//           </SwitchProvider>
+//         </ScreenProvider>
+//       </SidebarProvider>
+
+//       {/* 2. PLACÉ À LA RACINE EN COMPOSANT AUTO-FERMANT */}
+//       <ToastContainer 
+//         position="top-right"
+//         autoClose={3000}
+//         hideProgressBar={false}
+//         newestOnTop={false}
+//         closeOnClick
+//         rtl={false}
+//         pauseOnFocusLoss
+//         draggable
+//         pauseOnHover
+//         theme="light"
+//       />
+//     </AppProvider>
+//   )
+// }
 
 export default function App() {
   return (
-    <AppProvider>
-      <SidebarProvider>
-        <ScreenProvider>
-          <SwitchProvider>
-            {/* <AppRoutes /> */}
-            <div style={{fontSize:"40px", color:"green"}}>
-              APP ROUTES TEST OK
-            </div>
-          </SwitchProvider>
-        </ScreenProvider>
-      </SidebarProvider>
-
-      {/* 2. PLACÉ À LA RACINE EN COMPOSANT AUTO-FERMANT */}
-      <ToastContainer 
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </AppProvider>
+    <div style={{fontSize:"40px", color:"green"}}>
+      SANS PROVIDERS OK
+    </div>
   )
 }
