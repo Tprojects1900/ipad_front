5939140b48e85f9727c777939d4254a16ce54ff0
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-22 16:46:21 +0000

    transpilation resolved with success

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,51 @@
 import React from 'react'
 import { SwitchProvider } from './context/SwitchContext'
-import AppRoutes from './routes'
-import { ScreenProvider } from './context/ScreenContext'
-import { SidebarProvider } from './context/SidebarContext'
-import AppProvider from './providers/app.provider'
+// import AppRoutes from './routes'
+// import { ScreenProvider } from './context/ScreenContext'
+// import { SidebarProvider } from './context/SidebarContext'
+// mport AppProvider from './providers/app.provider'
 
 // // 1. IMPORTATION DES STYLES OBLIGATOIRE
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css' 
 
-export default function App() {
-  return (
-    <AppProvider>
-      <SidebarProvider>
-        <ScreenProvider>
-          <SwitchProvider>
-            <AppRoutes />
-            {/* <div style={{fontSize:"40px", color:"green"}}>
-              APP ROUTES TEST OK
-            </div> */}
-          </SwitchProvider>
-        </ScreenProvider>
-      </SidebarProvider>
-
-      {/* 2. PLACÉ À LA RACINE EN COMPOSANT AUTO-FERMANT */}
-      <ToastContainer 
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </AppProvider>
-  )
-}
-
 // export default function App() {
 //   return (
-//     // <AppProvider>
-//     <div style={{fontSize:"40px", color:"green"}}>
-//       SANS PROVIDERS OK avec useToggle(apollo client 29)
-//     </div>
-//     // </AppProvider>
+//     <AppProvider>
+//       <SidebarProvider>
+//         <ScreenProvider>
+//           <SwitchProvider>
+//             <AppRoutes />
+//             {/* <div style={{fontSize:"40px", color:"green"}}>
+//               APP ROUTES TEST OK
+//             </div> */}
+//           </SwitchProvider>
+//         </ScreenProvider>
+//       </SidebarProvider>
+
+//       {/* 2. PLACÉ À LA RACINE EN COMPOSANT AUTO-FERMANT */}
+//       <ToastContainer 
+//         position="top-right"
+//         autoClose={3000}
+//         hideProgressBar={false}
+//         newestOnTop={false}
+//         closeOnClick
+//         rtl={false}
+//         pauseOnFocusLoss
+//         draggable
+//         pauseOnHover
+//         theme="light"
+//       />
+//     </AppProvider>
 //   )
 // }
+
+export default function App() {
+  return (
+    // <AppProvider>
+    <div style={{fontSize:"40px", color:"green"}}>
+      SANS PROVIDERS OK avec useToggle(apollo client 30)
+    </div>
+    // </AppProvider>
+  )
+}
