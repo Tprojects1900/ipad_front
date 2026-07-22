c684c66790830127f288694cda44db022ae0e3ac
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-22 16:54:41 +0000

    new updated

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ export default function App() {
   return (
     // <AppProvider>
     <div style={{fontSize:"40px", color:"green"}}>
-      SANS PROVIDERS OK avec useToggle(apollo client 31)
+      SANS PROVIDERS OK avec useToggle(apollo client 32)
     </div>
     // </AppProvider>
   )
